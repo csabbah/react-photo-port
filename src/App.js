219d9889142a5c39b0af7761dace33005d9323e7
Csabbah <csabbah@ryerson.ca>
2022-06-26 20:57:23 -0400

    Added new Nav and About components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <Nav />
+        {/* The 'string' prop gets passed into the About comp. In the About comp, we
+        can restructure the string data (which is 'Who am I?') and use it */}
+        <About string={'Who am I?'} />
+      </main>
+    </>
   );
 }
-
 export default App;
